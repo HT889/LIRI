08320e04779ba8a3de58f32454951f6e4619c4db
Haig Talarian <haigtalarian@gmail.com>
2019-07-12 10:14:24 -0700

    3 functions working properly, still working on 4th

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var fs = require("fs");
 var AsciiTable = require('ascii-table')
 
 var keys = require("./keys.js");
@@ -10,36 +11,46 @@ const spotify = new Spotify(keys.spotify);
 
 var moment = require("moment");
 
-const action = process.argv[2];
+var action = process.argv[2];
+var parameter = process.argv.slice(3).join(" ");
+console.log(action);
 
+
+function liri() {
 switch (action) {
   case "concert-this":
-    concertThis();
+    concertThis(parameter);
     break;
   
   case "spotify-this-song":
-    spotifyThis();
+    spotifyThis(parameter);
     break;
 
   case "movie-this":
       movieThis();
       break;
-};
 
+  case "do-what-it-says":
+      doWhatItSays();
+      break;
+};
+}
+liri();
+//CONCERT THIS
 function concertThis() {
   const artistName = process.argv.slice(3).join(" ");
 
   let queryUrl = `https://rest.bandsintown.com/artists/${artistName}/events?app_id=codingbootcamp`;
   // console.log(queryUrl);
   axios.get(queryUrl).then(function (response) {
-    console.log(response.data);
-
+    console.log("Venue:", response.data[0].venue.name);
+    console.log("Location:", response.data[0].venue.city +",", response.data[0].venue.country);
+    console.log("Date:", moment(response.data[0].datetime).format("MMM Do YYYY"));
   }).catch(function (err) {
     console.log(err);
   }) 
 }
-
-
+//SPOTIFY THIS
 function spotifyThis() {
 
   const query = process.argv.slice(3).join(" ");
@@ -55,18 +66,14 @@ spotify.search({ type: 'track', query: query, limit: 3 })
       console.log("-------------------------------------------------");
       console.log("\n");
     }
-    // console.log(response.tracks);
-    // console.log(response.tracks.items[0].name);
-    // console.log(response.tracks.items[0].preview_url);
-    // console.log(response.tracks.items[0].album.name)
   })
   .catch(function(err) {
     console.log(err);
   });
 }
-
+//MOVIE THIS
 function movieThis() {
-  const movieName = process.argv[3];
+  const movieName = process.argv.slice(3).join(" ");
   let queryUrl = `http://www.omdbapi.com/?t=${movieName}&apikey=trilogy`;
 
   axios.get(queryUrl).then(function (response) {
@@ -82,8 +89,23 @@ function movieThis() {
     console.log("\n");
     console.log(response.data.Plot);
     console.log("\n");
-
   }).catch(function (err) {
     console.log(err);
   })
 }
+
+function doWhatItSays () {
+fs.readFile("random.txt", "utf8", function(error, data) {
+  if (error) {
+    return console.log(error);
+  }
+  console.log(data);
+
+  var dataArr = data.split(",");
+  parameter = dataArr[1];
+  action = dataArr[0];
+  liri();
+
+});
+}
+
